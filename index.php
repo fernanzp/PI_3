60e9ad0b3eb4f7c710b1df2ed19807dd36efa95a
fernanzp <fperez23@ucol.mx>
2024-08-30 19:49:29 -0600

    First commit: adding HTML and CSS files

---
+++ index.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="assets/stylesheets/index_style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>END</title>
+    <link rel="shortcut icon" href="assets/img/logo.png" type="">
+</head>
+<body>
+
+    <!-- <header class="contenido-hd">
+        <!-- <div class="logo"></div>
+        <h1>End Non-Disparity</h1> -->
+    <!-- </header> -->
+    
+    <div class="nav-bg">
+        <div class="zona-izq">
+            <div class="logo"></div>
+            <h1>End Non-Disparity</h1>
+        </div>
+        <div class="cont-navbar">
+            <!-- <a href="">Inicio</a> -->
+            <a href="">Nosotros</a>
+            <a href="">Maneras de ayudar</a>
+            <a href="">Contenido</a>
+            <!-- <a href="">Noticias</a> -->
+            <div class="icono">
+                <a href="#"><i class="fa-solid fa-circle-user fa-2xl"></i></i></a>
+            </div>
+        </div>
+    </div>
+
+    <div class="video">
+        <center>
+            <img src="https://wp.es.aleteia.org/wp-content/uploads/sites/7/2018/01/web3-poor-poverty-man-street-mexico-proted-mcgrath-cc-by-nc-sa-2-0.jpg" alt="">
+        </center>
+    </div>
+
+    <div class="titulo-donativo">
+        <h2>Donaciones</h2>
+    </div>
+    
+    <section class="cont-donativo">
+        <div class="cont-info">
+            <h2>Temas referentes</h2>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos laborum asperiores ratione error animi alias fuga quibusdam rerum, nulla tempore consequuntur quaerat sunt voluptate? Eaque labore commodi iste impedit ipsam.
+            </p>
+        </div>
+        <div class="donativo">
+            <h2>Cantidad</h2>
+            <div class="boton-container">
+                <button class="boton" type="button">$100</button>
+                <button class="boton" type="button">$200</button>
+                <button class="boton" type="button">$500</button>
+            </div>
+            <div class="boton-container">
+                <button class="boton" type="button">$1000</button>
+                <button class="boton" type="button">$5000</button>
+                <button class="boton" type="button">$10000</button>
+            </div>
+            <br>
+            <input type="number" placeholder="Ingrese cantidad">
+            <button class="boton-aceptar">Aceptar</button>
+        </div>
+    </section>
+
+    <footer class="footer">
+        <div class="espacio-logo">
+            <div class="logo">
+
+            </div>
+        </div>
+        <div class="registro">
+            <h2>Vuélvete Socio</h2>
+            <a href="#">Conviértete en beneficiario</a>
+            <a href="#">Conviértete en donante</a>
+            <a href="#">Conviértete en voluntario</a>
+            <h2>¿No tienes una cuenta?</h2>
+            <a href="#">Quiero registrarme</a>
+        </div>
+        <div class="informacion">
+            <h2>Información de contacto</h2>
+            <p>Fundación: EducationNon-Disparity
+                Correo: info@EducationNon-Disparity.org
+                Teléfono: +54 91 123 4567
+                Horario de atención: Lunes a Viernes, 9:00 a.m - 10:00 p.m
+            </p>
+        </div>
+    </footer>
+</body>
+</html>
